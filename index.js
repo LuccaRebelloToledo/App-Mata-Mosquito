5b724f88664b4196339488a4c1d2cdafe53b68f1
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2022-07-12 12:37:48 -0300

    Valores randomicos

---
+++ index.js
@@ -0,0 +1,51 @@
+var altura = 0;
+var largura = 0;
+
+function recuperarPalcoJogo(){
+altura = window.innerHeight;
+largura = window.innerWidth;
+console.log(altura, largura)
+}
+
+function geraPosicao(){
+recuperarPalcoJogo();    
+var posicaoX = Math.floor(Math.random() * largura) - 90;
+var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX;
+posicaoY = posicaoY < 0 ? 0 : posicaoY;
+console.log(posicaoX, posicaoY)
+
+var mosquitoDOM = document.createElement('img');
+mosquitoDOM.src = './assets/images/mosquito.png'
+mosquitoDOM.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+mosquitoDOM.style.left = posicaoX + 'px';
+mosquitoDOM.style.top = posicaoY + 'px';
+mosquitoDOM.style.position = 'absolute';
+document.body.appendChild(mosquitoDOM)
+}
+
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3);
+
+    switch(classe){
+        case 0:
+            return 'mosquitoPequeno'
+            case 1:
+                return 'mosquitoMedio'
+                case 2:
+                    return 'mosquitoGrande'
+    }
+}
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random() * 2);
+
+    switch(classe){
+        case 0:
+            return 'ladoA'
+            case 1:
+                return 'ladoB'
+    }
+}
